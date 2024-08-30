4d601772ad2f6ae50cb81dc4ce7abe3ae78528ed
Benjamin <benni.steeg@icloud.com>
2024-08-30 19:46:23 +0200

    add alu to top-module

--- src/top.v
+++ src/top.v
@@ -26,6 +26,9 @@ wire [7:0]bus_in_5;
 wire [7:0]bus_in_6;
 wire [7:0]bus_in_7;
 
+wire [7:0]x_reg;
+wire [7:0]y_reg;
+
 // Control wires
 wire [2:0] bus_sel;
 
@@ -36,6 +39,10 @@ wire y_load;
 wire ar_load;
 wire ir_load;
 
+wire [1:0]alu_op;
+
+wire [4:0]address;
+
 // Components
 bus_mux bus_mux(bus_sel, 
                 bus_in_0, bus_in_1, bus_in_2, bus_in_3, bus_in_4, bus_in_5, bus_in_6, bus_in_7,
@@ -44,9 +51,12 @@ bus_mux bus_mux(bus_sel,
 
 register akku(bus_out, bus_in_0, clk, akku_load, rst_n);
 register pc(bus_out, bus_in_1, clk, pc_load, rst_n);
-register x(bus_out, bus_in_2, clk, x_load, rst_n);
-register y(bus_out, bus_in_3, clk, y_load, rst_n);
-register ar(bus_out, bus_in_4, clk, ar_load, rst_n);
+register x(bus_out, x_reg, clk, x_load, rst_n);
+register y(bus_out, y_reg, clk, y_load, rst_n);
+register ar(bus_out, address, clk, ar_load, rst_n);
 register ir(bus_out, bus_in_5, clk, ir_load, rst_n);
+
+alu alu(x_reg, y_reg, alu_op, rst_n, clk, bus_in_6);
+
     
 endmodule
