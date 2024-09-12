ff5f8cec0958e7e1b386e0e35899160d55661f3f
Benjamin <benni.steeg@icloud.com>
2024-09-12 21:19:58 +0200

    start cpu-state-machine

--- src/top.v
+++ src/top.v
@@ -1,62 +1,64 @@
-/*
-Because this project is made to represent the 8-Bit CPU i am building out of actual 74LS-Chips,
-this design may be unnecessaryly complicated and/or convoluted.
-*/
-
-/*
-This is the top-module of the UL8-CPU, which connects the different components with each other.
-
-Signals:
-clk     : clock-input
-*/
 module top (
     input clk,
-    input rst_n
+    input resetn,
+    input startn
 );
 
-// Data wires
-wire [7:0]bus_out;
+localparam RESET = 3'b000;
+localparam FETCH = 3'b001;
+localparam DECODE = 3'b010;
+localparam EXECUTE = 3'b011;
+localparam WRITEBACK = 3'b100;
+localparam HALT = 3'b101;
 
-wire [7:0]bus_in_0;
-wire [7:0]bus_in_1;
-wire [7:0]bus_in_2;
-wire [7:0]bus_in_3;
-wire [7:0]bus_in_4;
-wire [7:0]bus_in_5;
-wire [7:0]bus_in_6;
-wire [7:0]bus_in_7;
+reg state;
+reg sys_clk;
+reg [31:0]ctr;
 
-wire [7:0]x_reg;
-wire [7:0]y_reg;
+initial begin
+    state <= RESET;
+    sys_clk <= 0;
+end
 
-// Control wires
-wire [2:0] bus_sel;
+always @(posedge clk) begin
+    if (ctr < 2500000) ctr <= ctr + 1;
+    else begin
+        sys_clk <= ~sys_clk;
+        ctr <= 0;
+    end
+    if (!startn) state <= FETCH;
+end
 
-wire akku_load;
-wire pc_load;
-wire x_load;
-wire y_load;
-wire ar_load;
-wire ir_load;
+reg [7:0]ram[0:31];
+reg [7:0]pc;
+reg [7:0]akku;
+reg [7:0]param;
+reg [7:0]ir;
 
-wire [1:0]alu_op;
+always @(posedge sys_clk) begin
+    case (state)
+        RESET: 
+        begin
+            pc <= 0;
+            akku <= 0;
+            param <= 0;
+            ir <= 0;
+        end
 
-wire [4:0]address;
+        FETCH: 
+        begin 
+            ir <= ram[pc[4:0]];
+            state <= DECODE;
+        end
 
-// Components
-bus_mux bus_mux(bus_sel, 
-                bus_in_0, bus_in_1, bus_in_2, bus_in_3, bus_in_4, bus_in_5, bus_in_6, bus_in_7,
-                rst_n,
-                bus_out);
-
-register akku(bus_out, bus_in_0, clk, akku_load, rst_n);
-register pc(bus_out, bus_in_1, clk, pc_load, rst_n);
-register x(bus_out, x_reg, clk, x_load, rst_n);
-register y(bus_out, y_reg, clk, y_load, rst_n);
-register ar(bus_out, address, clk, ar_load, rst_n);
-register ir(bus_out, bus_in_5, clk, ir_load, rst_n);
-
-alu alu(x_reg, y_reg, alu_op, rst_n, clk, bus_in_6);
-
+        DECODE: 
+        begin
+            pc <= pc + 1;
+            param <= {3'b000, ir[4:0]};
+            state <= EXECUTE;
+        end
+        default: 
+    endcase
+end
     
 endmodule
