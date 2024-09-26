944efcd7748a048d00ac34a80d2f21b14a8d65f0
uykfh@student.kit.edu <benni.steeg@icloud.com>
2024-09-26 20:37:46 +0200

    read lines from file

--- ul8_asm.c
+++ ul8_asm.c
@@ -1,28 +1,54 @@
 #include<stdio.h>
 #include<string.h>
 
-#if !defined(ERROR)
-#define ERROR(x) (fprintf(stderr, x))
-#endif 
+typedef struct Inst {
+    struct Inst *next;
+    char op;
+    char arg;
+} Inst;
+
+typedef struct Label {
+    char *name;
+    struct Inst *inst;
+    struct Label *next;
+} Label;
 
 int main(int argc, char const *argv[])
 {
     // Check for correct number of arguments.
     if (argc == 1) {
-        ERROR("No argument provided.\n");
+        printf("No argument provided.\n");
         return 1;
     } else if (argc > 2){
-        ERROR("To many arguments provided.\n");
+        printf("To0 many arguments provided.\n");
         return 1;
     }
+
     // Check for .ul8 extension
     const char *path = argv[1];
     char *p = (char *)path;
     while (*(p++) != 0);
     p -= 5;
     if(strcmp(p, ".ul8") != 0) {
-        ERROR("Provided file needs to end with the .ul8 extension.\n");
+        printf("Provided file needs to end with the .ul8 extension.\n");
         return 1;
     }
+
+    //Open file
+    FILE *file = fopen(path, "r");
+    if (file == NULL) {
+        printf("File at %s cannot be opened.\n", path);
+        return 1;
+    }
+
+    // Reads lines from file. Limited to 256 chars per line.
+    // TODO: Make length of lines irrelevant
+    char line[256];
+
+    while (fgets(line, 256, file) != NULL) {
+        printf("%s", line);
+    }
+
+    fclose(file);
     return 0;
 }
