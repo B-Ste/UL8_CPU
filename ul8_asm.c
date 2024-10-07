be6ee71f1ec846e6fa916724dba6f45c0b463ffc
uykfh@student.kit.edu <benni.steeg@icloud.com>
2024-10-07 13:04:37 +0200

    create data-structure for code-generation

--- ul8_asm.c
+++ ul8_asm.c
@@ -1,3 +1,4 @@
+#include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
 
@@ -8,11 +9,12 @@
 #endif
 
 #define LINE_LENGTH 256
+#define TOKEN_DELIMETER (" \n")
 
 typedef struct Inst {
     struct Inst *next;
-    char op;
-    char arg;
+    char *op;
+    char *arg;
 } Inst;
 
 typedef struct Label {
@@ -49,19 +51,50 @@ int main(int argc, char const *argv[])
         return 1;
     }
 
-    // Reads lines from file. Limited to 256 chars per line.
-    // TODO: Make length of lines irrelevant
-    char line[LINE_LENGTH];
+    // Initialize data-structure for code-generation
+    Inst *ih;
+    Inst *it;
 
+    Label *lh;
+    Label *lt;
+
+    // Reads lines from file. Limited to 256 chars per line.
+    char line[LINE_LENGTH];
     while (fgets(line, LINE_LENGTH, file) != NULL) {
         size_t len = strlen(line);
         if (line[len - 1] == '\n') line[len - 1] = 0;
         DEBUG_PRINT("Processing line: %s\n", line);
-        char *tk = strtok(line, " \n");
+        char *tk = strtok(line, TOKEN_DELIMETER);
         if (tk != NULL) {
             if (tk[strlen(tk) - 1] == ':') {
                 DEBUG_PRINT("Identified as label-line\n");
+                Label *l = malloc(sizeof(Label));
+                l->name = malloc(strlen(tk));
+                strcpy(l->name, tk);
+                if (lh == NULL) {
+                    lh = l;
+                    lt = l;
+                } else {
+                    lt->next = l;
+                    lt = l;
+                }
             } else {
+                Inst *i = malloc(sizeof(Inst));
+                i->op = malloc(strlen(tk));
+                strcpy(i->op, tk);
+                tk = strtok(NULL, TOKEN_DELIMETER);
+                if (tk != NULL) {
+                    i->arg = malloc(strlen(tk));
+                    strcpy(i->arg, tk);
+                }
+                if (ih == NULL) {
+                    ih = i;
+                    it = i;
+                } else {
+                    it->next = i;
+                    it = i;
+                }
+                if (lt->inst == NULL) lt->inst = i;
                 DEBUG_PRINT("Identified as instruction-line\n");
             }
         } else DEBUG_PRINT ("Ignored empty line\n");
